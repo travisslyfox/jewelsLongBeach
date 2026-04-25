e36fc2c4af8d3766c15c9b325a5767b3822c42ae
Travis Fox <95828919+travisslyfox@users.noreply.github.com>
2026-04-24 19:59:03 -0700

    youtube hotfix 2
    possible fix for youtube embed w/o ssl cert

--- jewelslongbeach.client/components/LBPVideo.tsx
+++ jewelslongbeach.client/components/LBPVideo.tsx
@@ -9,7 +9,7 @@ function LBPVideo() {
                 <iframe
                     width="100%"
                     height="100%"
-                    src="https://www.youtube.com/embed/p_p5kTSPJKY"
+                    src="http://www.youtube.com/embed/p_p5kTSPJKY"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
